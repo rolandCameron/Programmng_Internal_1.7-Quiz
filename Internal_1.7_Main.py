2a92e61d34265fc009fe21b9514cdc741e2abb9e
rolandCameron <99928788+rolandCameron@users.noreply.github.com>
2022-03-21 12:15:14 +1300

    Decomposition of the Program
    A detailed decomposition of the program, covering the entire main loop

---
+++ Internal_1.7_Main.py
@@ -0,0 +1,36 @@
+#IMPORTS
+
+#VARIABLES
+
+#FUNCTIONS
+
+#MAIN
+
+
+#DECOMPOSITION
+"""
+1. Present options for the topic of the quiz
+    a. Create a pygame screen
+    b. Present buttons for each of the answers and questions available (Flag - Capital, Capital - Flag, Country - Flag, etc...)
+    c. Detect a click on these buttons
+    d. Have the user confirm their selection, do not let them select the same answer and question eg. (flag - flag, capital - capital)
+
+2. Allow users to choose the are they want to cover (eg. Asia, Africa, Europe), the number of questions they want to answer, and how they want to answer (Multiple choice, written response)
+    a. Present a new screen of options, as detailed above
+    b. Allow the user to select one option from each field (maybe multiple continents?)
+    c. Allow the user to confirm their choices
+
+3. Present questions one by one, with the desired answer option
+    a. Present a question, with the related images
+    b. Allow the user to answer in their chosen way
+    c. Once they confirm their answer, move onto the next question
+
+4. Once all the questions have been answered, give the user a score
+    a. Present an end screen
+    b. Show the user their score and percentage they got correct
+    c. Display their answers, and which ones were correct
+
+5. Ask if the user wants to do another quiz
+    a. Allow the user to decide whether or not they want to play again
+    b. Let them choose either the same quiz, or to change some of the parameters
+"""
