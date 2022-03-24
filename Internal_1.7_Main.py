1f377b494eb093c1ea1264b99b077093d8baf3f9
rolandCameron <99928788+rolandCameron@users.noreply.github.com>
2022-03-25 12:40:01 +1300

    Buttons appear for each answer option
    There are now buttons asking a player how they want to answer on the settings screen. (Flag, Capital, Location)

--- Internal_1.7_Main.py
+++ Internal_1.7_Main.py
@@ -1,9 +1,20 @@
 #IMPORTS
 import pygame # Used for game window
 import sys # Used to end program
+import numpy as np
 
 #INITIALIZATION
 pygame.init() 
+
+#CLASSES
+class Colours: # Colours to be used within the project
+    BACKGROUND_C = pygame.Color(1, 117, 15)
+    TEXT_C = pygame.Color(0, 0, 0)
+    QUESTION_C = pygame.Color(1, 51, 117, 125)
+
+class Fonts: # Fonts to be used within the project
+    TITLE_F = pygame.font.SysFont('Comic Sans MS', 50)
+    QUESTION_F = pygame.font.SysFont('Comic Sans MS', 30)
 
 #VARIABLES
 monitorInfo = pygame.display.Info()
@@ -11,11 +22,30 @@ clock = pygame.time.Clock() # A clock that keeps track of game time
 fps = 30 # The number of frames a second the game is running at
 screen = pygame.display.set_mode((monitorInfo.current_w, monitorInfo.current_h - 20)) # The screen the game is drawn on to
 
+page = "Settings" # The page displayed to the user
+
+settingsTitle = Fonts.TITLE_F.render('Select your question and answer types', False, (Colours.TEXT_C)) # The settings title
+
+# An array with all the settings buttons in it
+settingsButtons = np.array([[Fonts.TITLE_F.render('Location', False, (Colours.TEXT_C)), Fonts.TITLE_F.render('Flag', False, (Colours.TEXT_C)), Fonts.TITLE_F.render('Capital', False, (Colours.TEXT_C))], [Fonts.TITLE_F.render('Location', False, (Colours.TEXT_C)), Fonts.TITLE_F.render('Flag', False, (Colours.TEXT_C)), Fonts.TITLE_F.render('Capital', False, (Colours.TEXT_C))]])
+
 #FUNCTIONS
 
 #MAIN
 while True:
-    screen.fill((0, 0, 0)) # Initialises the screen as plain black
+    screen.fill(Colours.BACKGROUND_C) # Initialises the screen as the background colour
+
+    if page == "Settings":
+        # Draws a rectangle behind the title
+        pygame.draw.rect(screen, Colours.QUESTION_C, (screen.get_width()/2 - settingsTitle.get_width()/2, 25, screen.get_width()/4 + settingsTitle.get_width()/2, 25 + settingsTitle.get_height()), 0, -1, 100, 100, 100, 100) 
+        screen.blit(settingsTitle, ((screen.get_width()/2 - settingsTitle.get_width()/2) + 2, 26)) # Writes the title on top of the rectangle
+
+        for y in range(3): 
+            for x in range(2):
+                topLeftX = (screen.get_width()/4)*(x + 1)
+                topLeftY = screen.get_height() - (screen.get_height()/4)*(y+1)
+                pygame.draw.rect(screen, Colours.QUESTION_C, (topLeftX, topLeftY, settingsButtons[x, y].get_width() + 10, settingsButtons[x, y].get_height() + 10), 0, -1, 50, 50, 50, 50)
+                screen.blit(settingsButtons[x, y], (topLeftX, topLeftY))
 
     for event in pygame.event.get(): # Runs for each possible event in pygame
         if event.type == pygame.QUIT:
